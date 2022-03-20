4771b2d660d7fb172e6cf0144bb406a7c721311e
KimKyungJi <kkj2551@naver.com>
2022-03-20 21:11:03 +0900

    [feat] localNotification 추가

--- client/App.js
+++ client/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
-import {View, Text, Alert} from 'react-native';
+import {View, Text} from 'react-native';
 import messaging from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   //앱에서 벗어난 상태이거나, 앱을 종료시킨 상태에서 메시지를 수신했을 때 이벤트 발생
@@ -10,8 +11,15 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
 const App = () => {
   useEffect(() => {
     const msg = messaging().onMessage(async remoteMessage => {
-      console.log(remoteMessage);
-      Alert.alert('A new FCM message arrived', JSON.stringify(remoteMessage));
+      //앱을 실행 중인 상태에서 메시지를 수신했을 때 이벤트 발생
+      //channelId 부분 required but 역할을 모르겠음
+      PushNotification.localNotification({
+        channelId: 'com.client',
+        message: remoteMessage.notification.body,
+        title: remoteMessage.notification.title,
+        bigPictureUrl: remoteMessage.notification.android.imageUrl,
+        smallIcon: remoteMessage.notification.android.imageUrl,
+      });
     });
     return msg;
   }, []);
