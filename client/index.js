7f79b17ce52c9e40f479512511ca0dc4a4240ef7
KimKyungJi <kkj2551@naver.com>
2022-03-21 09:20:41 +0900

    [fix] solve background issue

--- client/index.js
+++ client/index.js
@@ -1,5 +1,15 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import PushNotification, {Importance} from 'react-native-push-notification';
+
+PushNotification.createChannel(
+  {
+    channelId: 'com.client',
+    channelName: 'com.client',
+    importance: Importance.HIGH,
+  },
+  created => console.log(`createChannel return ${created}`),
+);
 
 AppRegistry.registerComponent(appName, () => App);
--- server/routes/index.js
+++ server/routes/index.js
@@ -17,6 +17,9 @@ router.get("/", (req, res) => {
       body: "world",
     },
     token: devicetoken,
+    android: {
+      priority: "high",
+    },
   };
   admin
     .messaging()
